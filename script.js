40e8fd2e7766e555c31e6ca2f28d1b03ae12b3f7
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2024-04-17 12:11:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+
+
+
+const btn = document.getElementById("btn")
+btn.addEventListener("click" , async () => {
+  let text = document.getElementById("text").value;
+  let delay = document.getElementById("delay").value;
+  await displayMessage(text , delay);
+})
+
+
+async function displayMessage(text , delay){
+
+  const delayTime = parseInt(delay);
+ await new Promise ((resolve , reject) => {
+    setTimeout(()=> {
+      resolve(document.getElementById("output").innerText = text)
+    }, delayTime*1000);
+
+  })
+
+
+}
